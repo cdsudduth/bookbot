ce305d0e276613be3560935988d6d29add335c80
cdsudduth <colesudduth@outlook.com>
2024-02-16 00:07:49 -0500

    Final draft of BookBot

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ def __main__():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     letter_count = get_letter_count(text)
-    print(f"{num_words} words found in document")
-    print(f"These are the totals of each letter found in the document: {letter_count}")
+    list_of_dicts = dict_conversion(letter_count)
+    sorted_letter_count = sort_dict(list_of_dicts)
+    print(f"---Begin report of {book_path}---")
+    print(f"{num_words} words found in document\n")
+    for x in sorted_letter_count:
+        print(f"The '{x[0]}' character was found {x[1]} times")
+
 
 def get_letter_count(text):
     letter_dict = {
@@ -47,12 +52,26 @@ def get_letter_count(text):
             if check_letter:
                 letter_dict[character] += 1
     return letter_dict
+
+
 def get_num_words(text):
     words = text.split( )
     return len(words)
+
 
 def get_book_text(path):
     with open(path) as book:
         book_content = book.read()
         return book_content
+
+def dict_conversion(dict):
+    keys = dict.keys()
+    counts = dict.values()
+    merged_list = list(zip(keys,counts))
+    #print(merged_list)
+    return merged_list
+def sort_dict(list):
+    new_list = sorted(list, key = lambda x: int(x[1]), reverse=True)
+
+    return new_list
 __main__()
