003d190f5acfa6d51fa54d5d3a0f787bcacf61ce
Cole <cole@Coles-MacBook-Air.local>
2024-02-15 20:03:08 -0500

    Added main functionality, get_num_words(), and get_book_text()

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-with open("Books/Frankenstein.txt")
+def __main__():
+    book_path = "Books/Frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in document")
+
+def get_num_words(text):
+    words = text.split( )
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as book:
+        book_content = book.read()
+        return book_content
+__main__()
